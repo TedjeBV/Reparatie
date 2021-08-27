815b4c5680cc7320f4d2aefc73e0ba31ff13d8d4
Thijs Boom <twboom@hotmail.nl>
2021-08-27 23:21:41 +0200

    Started renderer

--- js/catalog.js
+++ js/catalog.js
@@ -1,11 +1,37 @@
 // Session object to store data
 const session = {};
 
-// Renderer function
-function render(data) {}
+session.lang = 'nl';
+
+// Renderer functions
+function render(data) {
+
+    data[session.lang].forEach(category => {
+        const container = renderCategory(category);
+        document.getElementById('categories').appendChild(container);
+    });
+
+};
+
+function renderCategory(category) {
+
+    // Container
+    const container = document.createElement('div');
+    container.classList.add('category-container');
+
+    // Header
+    const header = document.createElement('h3');
+    header.innerText = category.type;
+    container.appendChild(header);
+
+    return container;
+
+};
 
 // Main function
 function main() {
+
+    render(session.guides);
 
 };
 
