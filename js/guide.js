8ef63fc7020ab7269b49f836334c434d3c352ca5
Thijs Boom <twboom@hotmail.nl>
2021-08-28 00:09:20 +0200

    Getting guide info and sending it to the renderer

--- js/guide.js
+++ js/guide.js
@@ -3,10 +3,27 @@ const session = {};
 
 session.lang = 'nl';
 
-session.guide = (new URL(document.location)).searchParams.get('guide');
+session.guide = decodeURI((new URL(document.location)).searchParams.get('guide'));
+
+// Renderer
+function render(guide, info) {
+
+
+
+}
 
 // Main function
 function main() {
+
+    // Get correct guide from data
+    const split = session.guide.split('-');
+    let guide = session.data[session.lang];
+    guide = guide.find( ({ type }) => type === split[0].toUpperCase());
+    guide = guide.categories.find( ({ type }) => type === split[1].toUpperCase());
+    guide = guide.files.find( ({ content }) => content === split[2].toUpperCase());
+
+    console.log(guide)
+    render(session.guideText, guide)
 
 };
 
@@ -15,6 +32,7 @@ function main() {
 // Files to load
 const promises = [
     fetch('assets/guides/guides.json').then(r => r.json()).then(json => session.data = json),
+    fetch('assets/guides/' + session.lang + '/' + session.guide + '.md').then(r => r.text()).then(md => session.guideText = md),
 ];
 
 // Run if all files are loaded
